9b104831bea98a1a0608128c38dfeac1848cf412
Shania Lopez <slopez@fandm.edu>
2024-05-14 16:50:02 -0400

    lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,13 +1,29 @@
-class Pet:
+class Owner:
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+       return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception('Not a pet instance')
+        pet.owner = self
+    
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key = lambda pet: pet.name)
 
+
+class Pet:
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
     all = []
-    def __init__(self, name, pet_type, owner = None):
+
+    def __init__(self, name, pet_type, owner=""):
         self.name = name
-        self.pet_type = pet_type
+        self. pet_type = pet_type
         self.owner = owner
         Pet.all.append(self)
-    
+
     @property
     def pet_type(self):
         return self._pet_type
@@ -17,28 +33,4 @@ class Pet:
         if pet_type not in self.PET_TYPES:
             raise Exception('Not a valid pet type.')
         self._pet_type = pet_type
-    
-    @property
-    def owner(self):
-        return self._owner
-    
-    @owner.setter
-    def owner(self, owner):
-        if not (isinstance(owner, Owner) or not owner):
-            raise Exception("Not type of owner")
-        self._owner = owner
 
-class Owner:
-    def __init__(self, name):
-        self.name = name
-    
-    def pets(self):
-        return [pet for pet in Pet.all if pet.owner == self]
-    
-    def add_pet(self, pet):
-        if not isinstance(pet, Pet):
-            raise Exception('Not type of pet')
-        pet.owner = self
-    
-    def get_sorted_pets(self):
-        return sorted(self.pets(), key = lambda pet: pet.name)
